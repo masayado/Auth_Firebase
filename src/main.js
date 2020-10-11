7de5a44c669fafddaa10cd83b57a3ef1291810f1
masayado <masayado@uc.cl>
2020-10-11 14:27:15 -0300

    instale y llame a firebase en el main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDhKkCbbH8zAou1DiB58pYf09bIT8qEaNc",
+    authDomain: "desafio-auth-login.firebaseapp.com",
+    databaseURL: "https://desafio-auth-login.firebaseio.com",
+    projectId: "desafio-auth-login",
+    storageBucket: "desafio-auth-login.appspot.com",
+    messagingSenderId: "489902870653",
+    appId: "1:489902870653:web:1677beefee22524cf42114"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
