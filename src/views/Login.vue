93fd7e607e2ed17f0d24ffef452c52295f898a71
masayado <masayado@uc.cl>
2020-10-11 15:11:33 -0300

    cree vista login con formulario para ingresar

---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <form>
+            <h2>Login</h2>
+            <label for="user">Usuario</label>
+            <input type="text" placeholder="Usuario" v-model="user"><br>
+            <label for="user">Contrasena</label>
+            <input type="password" v-model="password"><br>
+            <button @click.prevent="login">Ingresar</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+export default {
+    name: 'login-component',
+    // props: {},
+    data: function(){
+        return {
+            user:"",
+            password:"",
+        }
+    },
+    // computed: {},
+    methods: {
+        login:function(){
+        
+        },
+    },
+    //components: {},
+}
+</script>
+
+<style scoped>
+    
+</style>
